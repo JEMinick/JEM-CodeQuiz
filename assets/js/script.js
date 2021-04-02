d9de78ca26c6c8937ed10ce8fcc55c042331f9b1
J. E. Minick <james.minick@prodigy.net>
2021-04-02 15:14:06 -0700

    Modified to allow the question to reference an example.

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,8 +6,8 @@ var bQuizCompleted = false;
 var iCurrentQuestion = 0;
 var iCorrectAnswers = 0;
 var sAnswerChoiceList;
-var iAllottedTime = 75;
-var iSecondsLeft = (iAllottedTime+1);
+var iAllottedTime = 0;
+var iSecondsLeft = 0;
 var iPenaltySeconds = 5;
 
 // Used to let the user know how much time remains:
@@ -25,32 +25,238 @@ var mainForm = document.querySelector("#main-form");
 // THE array of all questions, possible choices for each question, 
 // and the correct answer:
 
-var aQuestions = [
+var a5Questions = [
   {
     sQuestion: "Commonly used data types DO NOT include:",
+    sQExample: "",
     aChoices: ["strings", "booleans", "alerts", "numbers"],
     sAnswer: "alerts"
   },
   {
     sQuestion: "The condition in an if / else statement is enclosed within [__?__]",
+    sQExample: "",
     aChoices: ["quotes", "curly brackets", "parentheses", "square brackets"],
     sAnswer: "parentheses"
   },
   {
     sQuestion: "Arrays in Javascript can be used to store [__?__].",
+    sQExample: "",
     aChoices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
     sAnswer: "all of the above"
   },
   {
     sQuestion: "String values must be enclosed within [__?__] when being assigned to variables.",
+    sQExample: "",
     aChoices: ["commas", "curly brackets", "quotes", "parenthesis"],
     sAnswer: "quotes"
   },
   {
     sQuestion: "A very useful tool for used during development and debugging for printing content to the debugger is:",
+    sQExample: "",
     aChoices: ["Javascript", "terminal / bash", "for loops", "console log"],
     sAnswer: "console log"
+  }
+];
+
+var a25Questions = [
+  { 
+    sQuestion: "Inside which HTML element do we put the JavaScript?",
+    sQExample: "",
+    aChoices: ["<scripting>","<script>","<javascript>","<js>"],
+    sAnswer: "<script>"
   },
+  {
+    sQuestion: "What is the correct JavaScript syntax to change the content of the following HTML element?",
+    sQExample: "   <p id=\"demo\">This is a demonstration.</p>",
+    aChoices: ["#demo.innerHTML = \"Hello World!\"",
+              "document.getElementByName( \"p\" ).innerHTML = \"Hello World!\"!",
+              "document.getElementById( \"demo\" ).innerHTML = \"Hello World!\"",
+              "document.getElement( \"p\" ).innerHTML = \"Hello World!\""],
+    sAnswer: "document.getElementById( \"demo\" ).innerHTML = \"Hello World!\""
+  },
+  {
+    sQuestion: "Where is the correct place to insert a JavaScript?",
+    sQExample: "",
+    aChoices: ["The <body> section","The <head> section","Both the <head> section and the <body> section"],
+    sAnswer: "Both the <head> section and the <body> section"
+  },
+  {
+    sQuestion: "What is the correct syntax for referring to an external script called \"xxx.js\"?",
+    sQExample: "",
+    aChoices: ["<script href=\"xxx.js\">","<script name=\"xxx.js\">","<script src=\"xxx.js\">"],
+    sAnswer: "<script src=\"xxx.js\""
+  },
+  {
+    sQuestion: "The external JavaScript file must contain the <script> tag.",
+    sQExample: "",
+    aChoices: ["False","True"],
+    sAnswer: "False"
+  },
+  {
+    sQuestion: "How do you write \"Hello World\" in an alert box?",
+    sQExample: "",
+    aChoices: ["alertBox(\"Hello World\");",
+               "msgBox(\"Hello World\");",
+               "alert(\"Hello World\");",
+               "msg(\"Hello World\");"],
+    sAnswer: "alert(\"Hello World\");"
+  },
+  {
+    sQuestion: "How do you create a function in JavaScript?",
+    sQExample: "",
+    aChoices: ["function = myFunction()","function.myFunction()","function myFunction()"],
+    sAnswer: "function myFunction()"
+  },
+  {
+    sQuestion: "How do you call a function named \"myFunction\"?",
+    sQExample: "",
+    aChoices: ["myFunction()","call function myFunction()","call myFunction()"],
+    sAnswer: "myFunction()"
+  },
+  {
+    sQuestion: "How to write an IF statement in JavaScript?",
+    sQExample: "",
+    aChoices: ["if (i == 5)","if i == 5","if i = 5 then","if i = 5"],
+    sAnswer: "if (i == 5)"
+  },
+  {
+    sQuestion: "How to write an IF statement for executing some code if \"i\" is NOT equal to 5?",
+    sQExample: "",
+    aChoices: ["if i <> 5","if i =! then","if (i <> 5)","if (i != 5)"],
+    sAnswer: "if (i != 5)"
+  },
+  {
+    sQuestion: "How does a WHILE loop start?",
+    sQExample: "",
+    aChoices: ["while (i <= 10; i++)","while (i <= 10)","while i = 1 to 10"],
+    sAnswer: "while (i <= 10)"
+  },
+  {
+    sQuestion: "How does a FOR loop start?",
+    sQExample: "",
+    aChoices: ["for (i <= 5; i++)","for (i=0; i <= 5; i++)","for (i=0; i <=5)","for i = 1 to 5"],
+    sAnswer: "for (i=0; i <= 5; i++)"
+  },
+  {
+    sQuestion: "How can you add a comment in a JavaScript?",
+    sQExample: "",
+    aChoices: ["//This is a comment","'This is a comment","<!--This is a comment-->"],
+    sAnswer: "//This is a comment"
+  },
+  {
+    sQuestion: "How to insert a comment that has more than one line?",
+    sQExample: "",
+    aChoices: ["/*This comment can span more than one line*/",
+               "//This comment can span more than one line//",
+               "<!--This comment can span more than one line-->"],
+    sAnswer: "/*This comment can span more than one line*/"
+  },
+  {
+    sQuestion: "What is the correct way to write a JavaScript array?",
+    sQExample: "",
+    aChoices: ["var colors = (1:\"red\", 2:\"green\", 3:\"blue\")",
+               "var colors = \"red\", \"green\", \"blue\"",
+               "var colors = 1 = (\"red\"), 2 = (\"green\"), 3 = (\"blue\")",
+               "var colors = [\"red\", \"green\", \"blue\"]"],
+    sAnswer: "var colors = [\"red\", \"green\", \"blue\"]"
+  },
+  {
+    sQuestion: "How do you round the number 7.25 to the nearest integer?",
+    sQExample: "",
+    aChoices: ["Math.round(7.25)","Math.rnd(7.25)","round(7.25)","rnd(7.25)"],
+    sAnswer: "Math.round(7.25)"
+  },
+  {
+    sQuestion: "How do you find the number with the highest value of x and y?",
+    sQExample: "",
+    aChoices: ["Math.max(x,y)","Math.ceil(x,y)","ceil(x,y)","top(x,y)"],
+    sAnswer: "Math.max(x,y)"
+  },
+  {
+    sQuestion: "What is the correct JavaScript syntax for opening a new window called \"w2\"?",
+    sQExample: "",
+    aChoices: ["w2 = window.new( \"http://www.github.com\" );",
+               "w2 = window.open( \"http://www.github.com\" );"],
+    sAnswer: "w2 = window.new( \"http://www.github.com\" );"
+  },
+  {
+    sQuestion: "JavaScript is the same as Java.",
+    sQExample: "",
+    aChoices: ["False","True"],
+    sAnswer: "False"
+  },
+  {
+    sQuestion: "How can you detect the client's browser name?",
+    sQExample: "",
+    aChoices: ["client.navName","navigator.appName","browser.name"],
+    sAnswer: "navigator.appName"
+  },
+  {
+    sQuestion: "Which event occurs when the user clicks on an HTML element?",
+    sQExample: "",
+    aChoices: ["onchange","onclick","onmouseclick","onmouseover"],
+    sAnswer: "onclick"
+  },
+  {
+    sQuestion: "How do you declare a JavaScript variable?",
+    sQExample: "",
+    aChoices: ["v carName;","var carName;","variable carName;"],
+    sAnswer: "var carName;"
+  },
+  {
+    sQuestion: "Which operator is used to assign a value to a variable?",
+    sQExample: "",
+    aChoices: ["*","-","x","="],
+    sAnswer: "="
+  },
+  {
+    sQuestion: "What will the following code return: Boolean(10 > 9)",
+    sQExample: "",
+    aChoices: ["NaN","false","true"],
+    sAnswer: "true"
+  },
+  {
+    sQuestion: "Is JavaScript case-sensitive?",
+    sQExample: "",
+    aChoices: ["Yes","No"],
+    sAnswer: "Yes"
+  }
+  // {
+  //   sQuestion: "",
+  //   sQExample: "",
+  //   aChoices: [">","","",""],
+  //   sAnswer: ""
+  // },
+  // {
+  //   sQuestion: "",
+  //   sQExample: "",
+  //   aChoices: [">","","",""],
+  //   sAnswer: ""
+  // },
+  // {
+  //   sQuestion: "",
+  //   sQExample: "",
+  //   aChoices: [">","","",""],
+  //   sAnswer: ""
+  // },
+  // {
+  //   sQuestion: "",
+  //   sQExample: "",
+  //   aChoices: [">","","",""],
+  //   sAnswer: ""
+  // },
+  // {
+  //   sQuestion: "",
+  //   sQExample: "",
+  //   aChoices: [">","","",""],
+  //   sAnswer: ""
+  // },
+  // {
+  //   sQuestion: "",
+  //   sQExample: "",
+  //   aChoices: [">","","",""],
+  //   sAnswer: ""
+  // },
 ];
 
 // =====================================================================================================
@@ -74,7 +280,7 @@ function getRandomInt(max) {
 
 // =====================================================================================================
 
-function createRandomizedQuestionList()
+function createRandomizedQuestionList(aQuestions)
 {
   var iTotalQuestions = aQuestions.length;
   var aNewQuestions = [];
@@ -115,7 +321,7 @@ function createRandomizedQuestionList()
 // In order to make the quiz a little more interesting,
 // shuffle the list of questions so they're not so
 // predictable when re-started:
-var aQuestions2 = createRandomizedQuestionList();
+var aQuestions2 = createRandomizedQuestionList(a25Questions);
 if ( bDebugging )
 {
   console.log( "The new shuffled question list is as follows:" );
@@ -142,7 +348,14 @@ function displayQuestion( iQuestionIndex )
     var sQuestionParagraph = aQuestions2[iQuestionIndex].sQuestion;
     return( sQuestionParagraph );
   }
-
+  
+  function createQuestionP2(iQuestionIndex)
+  {
+    var sQuestionParagraph = aQuestions2[iQuestionIndex].sQExample;
+    sQuestionParagraph = sQuestionParagraph.trim(sQuestionParagraph);
+    return( sQuestionParagraph );
+  }
+  
   function checkAnswer(event)
   {
     var element = event.target;
@@ -151,16 +364,18 @@ function displayQuestion( iQuestionIndex )
 
     if ( element.matches("li") )
     {
+      // Correct condition 
+      if ( element.textContent === aQuestions2[iQuestionIndex].sAnswer )
+      {
         // Correct condition 
-        if ( element.textContent === aQuestions2[iQuestionIndex].sAnswer ) {
-            iCorrectAnswers++;
-            quizStatusLine.textContent = "Your answer of: [" + aQuestions2[iQuestionIndex].sAnswer + "] was correct!";
-            // Correct condition 
-        } else {
-            // Will deduct from iSecondsLeft for wrong answers:
-            iSecondsLeft = iSecondsLeft - iPenaltySeconds;
-            quizStatusLine.textContent = "Wrong! The correct answer was [" + aQuestions2[iQuestionIndex].sAnswer + "]...";
-        }
+        iCorrectAnswers++;
+        // quizStatusLine.textContent = "Your answer of: [" + aQuestions2[iQuestionIndex].sAnswer + "] was correct!";
+        quizStatusLine.textContent = "Correct!";
+      } else {
+        // Wrong answer: Will deduct from iSecondsLeft for wrong answers:
+        iSecondsLeft = iSecondsLeft - iPenaltySeconds;
+        quizStatusLine.textContent = "Wrong! The correct answer was [" + aQuestions2[iQuestionIndex].sAnswer + "]...";
+      }
     }
     
     // Update the current global question number:
@@ -200,15 +415,22 @@ function displayQuestion( iQuestionIndex )
     sChoiceList += "</ul>";
 
     return( sChoiceList );
-  }
-
+    
+  } // endOfFunction: createChoiceList()
+  
   //--------------------------------------------------------------------------
-
+  
   if ( bDebugging )
-    console.log( `Question #${iCurrentQuestion}${1}:` );
-
+  {
+    //console.log( `Question #${iCurrentQuestion}${1}:` );
+    var iQNo = iCurrentQuestion+1;
+    console.log( "Question #" + iQNo + ":" );
+  }
+  
   //--------------------------------------------------------------------------
-  mainForm.innerHTML = ""
+
+  clearQuizForm();
+  
   var sQuestion = createQuestionP(iCurrentQuestion);
 
   if ( bDebugging )
@@ -223,6 +445,21 @@ function displayQuestion( iQuestionIndex )
 
   //--------------------------------------------------------------------------
 
+  var sQExample = createQuestionP2(iCurrentQuestion);
+  if ( sQExample.length > 0 )
+  {
+    if ( bDebugging )
+    {
+      console.log( "Question-Example:" );
+      console.log( "\t\"" + sQExample + "\"" );
+    }
+    var oNewParagraph = document.createElement("p" );
+    oNewParagraph.textContent = sQExample;
+    mainForm.appendChild( oNewParagraph );
+  }
+
+  //--------------------------------------------------------------------------
+  
   if ( bDebugging )
     console.log( "Un-ordered Choice List:" );
 
@@ -292,13 +529,6 @@ function quizCompleted()
         clearQuizForm();
         timeEl.textContent = "High Scores";
         quizStatusLine.textContent = "";
-        
-        // if ( bDebugging ) {
-        //   console.log( "mainForm.innerHTML:" );
-        //   console.log( mainForm.innerHTML );
-        //   // console.log( "elMainFormUL" );
-        //   // console.log( elMainFormUL.innerHTML );
-        // }
         
         var oNewParagraph = document.createElement("p" );
         oNewParagraph.setAttribute( "style", "text-align: center" );
@@ -518,13 +748,19 @@ function startTimer() {
 // function RunApp() {
 startBtn.addEventListener( "click", function() 
   {
-    iAllottedTime = 30;
+    // iAllottedTime = 50;  // a5Questions:  (5*10)
+    // iAllottedTime = 250; // a25Questions: (25*10)
+    iAllottedTime = aQuestions2.length*10;
     iSecondsLeft = (iAllottedTime+1);
-    mainForm.innerHTML = "";
+    //mainForm.innerHTML = "";
+    clearQuizForm();
     displayQuestion(iCurrentQuestion);
     quizStatusLine.textContent = "Click on the correct answer!";
     startTimer();
   }
 );
+
+var sCurrentBrowser = navigator.appName;
+console.log( "Running the app with: " + sCurrentBrowser );
 
 // RunApp();
